125560fa3a4094a848aaf8b677ec5a4fb4713137
DELISSE Manon <manon.delisse60@free.fr>
2015-12-17 15:30:40 +0100

    js page d'accueil + lien créés

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 function afficherTexteBd () {
 document.getElementById('bd_js').innerHTML =
-'<div style=\"background-color:#2DC800; border-radius: 45px; height:60px; padding:40px; padding-left:60px;\"> Découvrez ici l\'univers merveilleux de la bande-dessinée, par toutatis !</div>';
+'<div style=\"background-color:#2DC800; border-radius: 45px; height:50px; padding:40px; padding-top:50px; padding-left:36px;\"> Découvrez ici l\'univers de la bande-dessinée, par toutatis !</div>';
 }
 
 function afficherTexteManga () {
   document.getElementById('manga_js').innerHTML=
-  '<div style=\"background-color:#62A9FF; border-radius: 45px; height: 60px; padding:40px; padding-left:60px;\"> Plongez dans le monde varié du manga ! \"Gambatte\" !</div>';
+  '<div style=\"background-color:#62A9FF; border-radius: 45px; height: 50px; padding:40px; padding-left:40px;\"> Plongez dans le monde varié du manga ! \"Ganbatte\" !</div>';
 }
 
 function afficherTexteRoman () {
 document.getElementById('roman_js').innerHTML =
-'<div style=\"background-color:#DC143C; border-radius: 45px; height:60px; padding:40px; padding-left:60px; padding-bottom: 50px;\">Honneur aux romans et particulièrement aux épopées fantastiques que j\'affectione !</div>';
+'<div style=\"background-color:#DC143C; border-radius: 45px; height:50px; padding:40px; padding-left:39px; padding-bottom: 50px;\">Honneur aux romans et aux épopées fantastiques que j\'affectione particulièrement !</div>';
 }
 
 function enleverTexte () {
